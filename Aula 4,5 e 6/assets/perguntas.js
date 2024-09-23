396da5519dc08a72cfe4d14cf95cfe6525d2f001
Maria Pavanelo <144125190+tkymoon@users.noreply.github.com>
2024-09-23 08:16:32 -0300

    Add files via upload

---
+++ Aula 4,5 e 6/assets/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'ESTRELAS',
+    'O que varia a temperatura delas?',
+    'A temperaura de uma estrela varia de acordo com sua <span style="color: rgb(255, 255, 185);">massa e idade.</span>Estrelas Massivas têm temperaturas mais altas porque a pressão em seus núcleos é maior, o que acelera as reações de fusão nuclear.Estrelas jovens também tendem a ser mais quentes, enquanto as mais velhas, como as anãs vermelhas, são mais frias. A cor de uma estrela indica sua temperatura:Sestrelas azuis são muito quentes, enquantos as vermelhas são frias.'
+)
+
+criaCartao(
+    'BURACOS NEGROS',
+    'Como são explicados?',
+    'Buracos negros são regiões do espeçõ onde a gravidade é tão imensa que nada, nem mesmo luz, consegue escapar.Eles se formam quando uma estrela muito massiva explode em uma supernova e seu núcleo colapsa, comprimindo-se em um ponto extremamente denso chamado <span style="color: rgb(255, 72, 0);">"singularidade"</span>.Ao redor da singularidade, há uma área chamada <span style="color: rgb(255, 72, 0);">"horizonte de eventos"</span>, que é a fronteira além da qual nada pode voltar. O que chega perto o suficiente, pode ser puxado por sua gravidade extrema.</p>'
+)
+
+criaCartao(
+    'PLANETAS',
+    'Como funcionam as órbitas?',
+    'As órbitas dos planetas funcionam graças a um <span style="color: rgb(94, 214, 110);">equilíbrio entre a gravidade</span> do Sol e a <span style="color: rgb(94, 214, 110);">velocidade</span> dos planetas.A gravidade do Sol puxa os planetas em sua direção, mas como eles estão se movendo rapidamente, em vez de serem atraídos diretamente,eles entram em uma trajetória curva ao redor da estrela. Esse movimento contínuo faz com que os planetas fiquem presos em órbitas elípticas (levemente ovaladas).'
+)
