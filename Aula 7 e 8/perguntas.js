cea57383924bd95b1259ef3ca395b91a015da2e8
Maria Pavanelo <144125190+tkymoon@users.noreply.github.com>
2024-09-30 08:18:03 -0300

    Add files via upload

---
+++ Aula 7 e 8/perguntas.js
@@ -0,0 +1,6 @@
+
+criaCartao(
+    'NEBULOSAS',
+    'Como surgem e o que caracterizam seus formatos?',
+    'As nebulosas surgem da <span style="color: rgb(140, 161, 255);">acumulação de gás e poeira</span>, muitas vezes após explosões de estrelas.<br> Suas formas são definidas pela <span style="color: rgb(140, 161, 255);"> gravidade, rotação e a influência de estrelas próximas</span>.Por <br>exemplo, nebulosas de emissão apresentam formas irregulares devido à ionização do gás,<br> enquanto nebulosas planetárias,resultantes da expulsão de camadas externas de estrelas, <br>costumam ter formas mais esféricas. Estruturas filamentosas são frequentementeformadas <br>por choques e interações do gás com a radiação estelar, criando um visual intrigante no cosmos.'
+)
